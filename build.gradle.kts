56770105ce47d4799ef11026992571479a3d9cf6
Fionitos <fionitgaming@gmail.com>
2025-09-17 15:56:41 +0300

    set 17 java version in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,13 @@ plugins {
 }
 
 group = "me.fionitos.discord"
-version = "1.0.4"
+version = "1.0.5"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
 
 repositories {
     mavenCentral()
