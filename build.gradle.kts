e21d84b53a2ad4d9fb91f35713db81f3393e9b81
Fionitos <fionitgaming@gmail.com>
2025-09-17 15:42:30 +0300

    remove publishing in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
     id("java")
-    id("maven-publish")
 }
 
 group = "me.fionitos.discord"
-version = "1.0.3"
+version = "1.0.4"
 
 repositories {
     mavenCentral()
@@ -13,24 +12,4 @@ repositories {
 dependencies {
     implementation("org.jetbrains:annotations:23.0.0")
     implementation("com.google.code.gson:gson:2.10.1")
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-        }
-    }
-
-    repositories {
-        maven {
-            name = "arlekin-repo"
-            url = uri("https://repo.arlekin.space/releases")
-
-            credentials {
-                username = System.getenv("MAVEN_ARLEKIN_ALIAS")
-                password = System.getenv("MAVEN_ARLEKIN_TOKEN")
-            }
-        }
-    }
 }
